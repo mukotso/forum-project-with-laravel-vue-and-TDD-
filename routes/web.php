c61b736f391fc6fcbef3f24d1e3595bd4a4f6a7b
Mukotso <kelvinmukotso@gmail.com>
2022-02-06 11:57:23 +0300

    2-test driving threads

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ThreadsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::get('/threads', [ThreadsController::class, 'index']);
+Route::get('/threads/{thread}', [ThreadsController::class, 'show']);
